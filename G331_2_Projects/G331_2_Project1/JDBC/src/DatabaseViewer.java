2a128288430b89228c3611a9c2c198554cc42b39
Damaris Campos <114826798+DCampos22@users.noreply.github.com>
2024-03-13 17:01:37 -0400

    GUI

--- G331_2_Projects/G331_2_Project1/JDBC/src/DatabaseViewer.java
+++ G331_2_Projects/G331_2_Project1/JDBC/src/DatabaseViewer.java
@@ -1,11 +1,22 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.swing.*;
+import java.sql.*;
 
-public class Create_DB {
-    public static void main(String[] args) {
+public class DatabaseViewer extends JFrame {
+    private JTextArea resultArea;
+
+    public DatabaseViewer() {
+        super("Database Viewer");
+        resultArea = new JTextArea(10, 50);
+        resultArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(resultArea);
+
+        add(scrollPane);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); // Center the frame
+        pack();
+        setVisible(true);
+
         // Load the JDBC driver
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -16,8 +27,6 @@ public class Create_DB {
 
         // JDBC URL for SQL Server
         String url = "jdbc:sqlserver://localhost:13001;databaseName=Northwinds2022TSQLV7;encrypt=false;";
-
-
         String username = "sa";
         String password = "PH@123456789";
 
@@ -25,16 +34,22 @@ public class Create_DB {
             // Establish connection to the database
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to the database");
+
             // Use the connection to execute SQL queries
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT TOP 5 * FROM HumanResources.Employee");
+
+            // Display results in the GUI
+            StringBuilder resultText = new StringBuilder();
             while (resultSet.next()) {
-                // Retrieve and print values from the result set
                 int id = resultSet.getInt("EmployeeId");
                 String firstname = resultSet.getString("EmployeeFirstName");
                 String lastname = resultSet.getString("EmployeeLastName");
-                System.out.println("ID: " + id + ", First name: " + firstname + ", Last name: " + lastname);
+                resultText.append("ID: ").append(id).append(", First name: ").append(firstname)
+                        .append(", Last name: ").append(lastname).append("\n");
             }
+
+            resultArea.setText(resultText.toString());
 
             // Close the result set, statement, and connection
             resultSet.close();
@@ -45,5 +60,9 @@ public class Create_DB {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(DatabaseViewer::new);
     }
 }
